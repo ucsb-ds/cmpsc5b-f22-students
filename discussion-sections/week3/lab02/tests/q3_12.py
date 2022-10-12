0a4899ae9aeb894db9934cc344bcc7a011111189
Nagarjun Avaraddy <nagarjun@umail.ucsb.edu>
2022-10-12 14:50:05 -0700

    week3 material

---
+++ discussion-sections/week3/lab02/tests/q3_12.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q3_12', 'points': None, 'suites': [{'cases': [{'code': '>>> empirical_P > 0.05\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ discussion-sections/week3/lab02/tests/q3_9.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_9',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> np.isclose(round(find_test_stat(change_in_death_rates, "Death Penalty", "Murder Rate"),3) - 0.607, 0)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
