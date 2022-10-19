63831ae0e2124c04f99aca207667117d729bf7e3
Nagarjun Avaraddy <nagarjun@umail.ucsb.edu>
2022-10-19 14:32:48 -0700

    week4 material

---
+++ discussion-sections/week4/tests/q2_4.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_4',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> 1 <= restaurants_tied <= 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
