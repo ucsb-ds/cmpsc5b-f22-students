bb590334d480cfe5e553a74d6a0e7c098647e4d6
Nagarjun Avaraddy <nagarjun@umail.ucsb.edu>
2022-10-26 14:33:36 -0700

    week5 midterm project

---
+++ discussion-sections/week5 (midterm project)/tests/q1_4.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_4',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(traffic_observed_statistic) == float\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ discussion-sections/week5 (midterm project)/tests/q1_7.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_7',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> accidents_p_value < .05\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
